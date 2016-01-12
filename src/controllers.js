c4127b4e45f4cf08fa9fa90e80b0ab7177282aeb
Ferdinand Fabrigas Jr <ferdie@oxygenventures.com.au>
2016-01-12 11:47:45 +0800

    POD-1091: Post controller

--- src/controllers.js
+++ src/controllers.js
@@ -4,7 +4,9 @@
 import * as getListController from './controllers/get-list-controller'; // eslint-disable-line no-unused-vars
 import * as getController from './controllers/get-controller'; // eslint-disable-line no-unused-vars
 import * as patchController from './controllers/patch-controller'; // eslint-disable-line no-unused-vars
+import * as postController from './controllers/post-controller'; // eslint-disable-line no-unused-vars
 
 export const getList = getListController;
 export const get = getController;
 export const patch = patchController;
+export const post = postController;
